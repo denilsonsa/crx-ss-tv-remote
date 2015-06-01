3697188f308f4a75093062be3f9f3cef6f2044ba
Denilson Sá <denilsonsa@gmail.com>
2015-06-01 09:23:15 -0300

    Adding a missing file.
    Really?! How could I forget commiting this file?

---
+++ options.js
@@ -0,0 +1,90 @@
+'use strict';
+
+var g_last_saved_options = {};
+
+function set_form_values_from_options(options) {
+	var form = document.getElementById('options_form');
+
+	form.tv_ip.value = options.tv_ip;
+	form.tv_port.value = options.tv_port;
+	form.unique_id.value = options.unique_id;
+	form.display_name.value = options.display_name;
+	form.macro_behavior.value = options.macro_behavior;
+
+	var submit_button = document.getElementById('submit_button');
+	submit_button.disabled = true;
+	g_last_saved_options = options;
+}
+
+function hide_toaster_message() {
+	var toaster = document.getElementById('toaster');
+	toaster.style.opacity = 0.0;
+	setTimeout(function() {
+		toaster.style.display = 'none';
+	}, 1000);
+}
+
+function show_toaster_message(message, timeout) {
+	var toaster = document.getElementById('toaster');
+	toaster.textContent = message;
+	toaster.style.display = 'block';
+	setTimeout(function() {
+		toaster.style.opacity = 1.0;
+		setTimeout(hide_toaster_message, timeout);
+	}, 10);
+}
+
+function form_element_input_handler(ev) {
+	var submit_button = document.getElementById('submit_button');
+	if (ev.target.value != g_last_saved_options[ev.target.name]) {
+		submit_button.disabled = false;
+	}
+}
+
+function form_submit_handler(ev) {
+	if (ev) {
+		ev.preventDefault();
+		ev.stopPropagation();
+	}
+
+	var form = document.getElementById('options_form');
+	var options = {
+		'tv_ip': form.tv_ip.value,
+		'tv_port': form.tv_port.value,
+		'unique_id': form.unique_id.value,
+		'display_name': form.display_name.value,
+		'macro_behavior': form.macro_behavior.value
+	}
+
+	chrome.runtime.getBackgroundPage(function(background) {
+		background.save_options_to_storage(options, function() {
+			var submit_button = document.getElementById('submit_button');
+			submit_button.disabled = true;
+			show_toaster_message('Saved!', 5000);
+			// TODO: Update TV_OPTS variable at the main window.
+		}, function(message) {
+			console.error(message);
+			alert(message);
+		});
+	});
+}
+
+function init() {
+	var form = document.getElementById('options_form');
+	form.addEventListener('submit', form_submit_handler);
+	form.addEventListener('input', form_element_input_handler);
+
+	var close_button = document.getElementById('close_button');
+	close_button.addEventListener('click', function(ev) {
+		window.close();
+	});
+
+	chrome.runtime.getBackgroundPage(function(background) {
+		background.get_options_from_storage(set_form_values_from_options, function(message) {
+			console.error(message);
+			alert(message);
+		});
+	});
+}
+
+init();
